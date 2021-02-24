0adc439726433964d25299ea156de91b54c37e14
chunwei <hcw1212@163.com>
2021-02-24 18:28:25 +0800

    更改ToastUtils

--- vandlib/src/main/java/com/cwand/lib/ktx/utils/ToastUtils.kt
+++ vandlib/src/main/java/com/cwand/lib/ktx/utils/ToastUtils.kt
@@ -1,9 +1,11 @@
 package com.cwand.lib.ktx.utils
 
 import android.content.Context
+import android.os.Looper
 import android.widget.Toast
 import androidx.annotation.MainThread
 import androidx.annotation.StringRes
+import com.cwand.lib.ktx.AndLib
 
 /**
  * @author : chunwei
@@ -35,5 +37,29 @@ object ToastUtils {
     @MainThread
     fun toastLong(context: Context, @StringRes messageRes: Int) {
         Toast.makeText(context, messageRes, Toast.LENGTH_SHORT).show()
+    }
+
+    fun safeToast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+        if (Thread.currentThread() != Looper.getMainLooper().thread) {
+            //子线程
+            Looper.prepare()
+            Toast.makeText(AndLib.getAppContext(), message, duration).show()
+            Looper.loop()
+        } else {
+            //主线程
+            Toast.makeText(AndLib.getAppContext(), message, duration).show()
+        }
+    }
+
+    fun safeToast(@StringRes messageRes: Int, duration: Int = Toast.LENGTH_SHORT) {
+        if (Thread.currentThread() != Looper.getMainLooper().thread) {
+            //子线程
+            Looper.prepare()
+            Toast.makeText(AndLib.getAppContext(), messageRes, duration).show()
+            Looper.loop()
+        } else {
+            //主线程
+            Toast.makeText(AndLib.getAppContext(), messageRes, duration).show()
+        }
     }
 }
